8ab7cd09355f238156807370569e557af5f606d7
Erick Osuna Garcia <127696393+Erickzzxc@users.noreply.github.com>
2025-09-30 14:05:58 -0700

    HUD & Planet Generation

---
+++ scripts/main_scene.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+
+func _on_bt_incio_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/GameScene.tscn")
+
+func _on_bt_opciones_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_bt_salir_pressed() -> void:
+	get_tree().quit()
