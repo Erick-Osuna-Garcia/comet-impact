a9a426825d657ce0d08434be1f64c8432a5f5bba
Erick Osuna Garcia <127696393+Erickzzxc@users.noreply.github.com>
2025-10-03 12:41:09 -0700

    Actualizacion de coasas

--- scenes/planet_terrestrial.gd
+++ scenes/planet_terrestrial.gd
@@ -12,20 +12,34 @@ func _ready():
 
 # Función cuando un cuerpo entra en el área
 func _on_body_entered(body: Node):
+<<<<<<< HEAD
 	if body is RigidBody3D and body.name == "Meteorito":
 		print("¡Impacto detectado en el planeta!")
 		_simulate_impact(body.global_position)
 		animate_crater()
 		#body.queue_free() # Elimina el meteorito tras impacto
+=======
+	if body is RigidBody3D and body.name == "Meteorite":
+		print("¡Impacto detectado en el planeta!")
+		_simulate_impact(body.global_position)
+		animate_crater()
+		body.queue_free() # Elimina el meteorito tras impacto
+>>>>>>> 6bafaee (Actualizacion de coasas)
 
 # Simula impacto en el shader
 func _simulate_impact(hit_position: Vector3):
 	# Convertir la posición a espacio local
 	var local_pos = to_local(hit_position)
+<<<<<<< HEAD
 	
 	shader_material.set_shader_parameter("impact_position", local_pos)
 	shader_material.set_shader_parameter("impact_radius", 100.5)
 	shader_material.set_shader_parameter("impact_strength", 3.0) # empieza en 0
+=======
+	shader_material.set_shader_parameter("impact_position", local_pos)
+	shader_material.set_shader_parameter("impact_radius", 0.5)
+	shader_material.set_shader_parameter("impact_strength", 0.0) # empieza en 0
+>>>>>>> 6bafaee (Actualizacion de coasas)
 
 # Animar crecimiento del cráter
 func animate_crater():
